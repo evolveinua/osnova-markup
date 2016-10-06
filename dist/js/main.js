00a4763f8d7873e13c8f2aff90bc74307cf96aa9
LevDavidow <nihilus.est@gmail.com>
2016-10-06 22:50:58 +0300

    Added filter, searchbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -138,7 +138,15 @@ document.addEventListener('DOMContentLoaded', function () {
         this._items.remove(this.current);
       }
       this.classList.remove(this._className + '_open');
+      this.current = undefined;
       this._unfreezeHtml(); 
+    }
+    toggle(className) {
+      if(this.current && this.current === className) {
+        this.close();
+      }else {
+        this.open(className);
+      }
     }
   }
   const modal = new Modal('.modal');
@@ -148,7 +156,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.menu-item-53 a').addEventListener('click',
     (e) => {
       e.preventDefault();
-      modal.open('modal__categories_open')
+      modal.toggle('modal__categories_open')
     }
   );
 
@@ -157,11 +165,19 @@ document.addEventListener('DOMContentLoaded', function () {
           .addEventListener('click',
           (e) => {
             e.preventDefault();
-            modal.open('modal__form_open')
+            modal.toggle('modal__form_open')
           }
   );
 
-  //Открыть форму поиска
+  //Вьюха формы поиска
+  document.getElementById('search-bar__close')
+          .addEventListener('click',
+          (e) => {
+            document.querySelector('html')
+                    .classList
+                    .toggle('header_search')
+          }
+  );
   document.getElementById('control-bar__item_search')
           .addEventListener('click',
           (e) => {
@@ -180,5 +196,53 @@ document.addEventListener('DOMContentLoaded', function () {
                       .toggle('control-bar__item_show-list');
           });
   
+  //Уменьшить верхнее меню.
+  class ScrollDirectionSpy {
+    // Если происходит скролл вверх - вызывает первый колбек 1 раз,
+    // Скролл вниз - второй коллбек 1 раз
+    constructor(callbackToTop, callbackToBottom) {
+      this.callbackToTop = callbackToTop;
+      this.callbackToBottom = callbackToBottom;
+
+      this._previousCallbackToTop = false;
+      this._previousScroll = 0;
+
+      document.addEventListener('scroll', this.watch.bind(this))
+    }
+    watch(event) {
+      const scroll = this.getBodyScrollTop();
+      if (scroll > this._previousScroll) {
+        this.handleScrollToTop();
+      }else {
+        this.handleScrollToBottom();
+      }
+      this._previousScroll = scroll;
+    }
+    getBodyScrollTop(){  
+     return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);  
+    }
+    handleScrollToTop() {
+      if(! this._previousCallbackToTop) {
+        this.callbackToTop();
+        this._previousCallbackToTop = !this._previousCallbackToTop;
+      }
+    }
+    handleScrollToBottom() {
+      if(this._previousCallbackToTop) {
+        this.callbackToBottom();
+        this._previousCallbackToTop = !this._previousCallbackToTop;
+      }
+    }    
+  }
   
+  const headerShrinker = new ScrollDirectionSpy(
+    () => document.querySelector('html')
+                  .classList
+                  .add('header-shrinked'),
+    () => document.querySelector('html')
+                  .classList
+                  .remove('header-shrinked') 
+  )
+
+
 })
